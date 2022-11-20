5f7cea0690fdef76b8d1aee7f93dab17d3c82621
Francois PIAT <fr.piat@hotmail.fr>
2022-11-20 22:33:22 +0100

    Add security on db file format encoding and content

--- Parser.cpp
+++ Parser.cpp
@@ -14,27 +14,26 @@ Parser::Parser()
 
 std::string Parser::getFirstDisplayNameItem()
 {
+    std::string nameItemStr;
     // std::string tmpBuff{};
     // std::ifstream dbStream("./database.json");
     // json j = json::parse(dbStream);
     // std::string line;
-    // while (std::getline(dbStream, line))
-    // {
-    //     std::cout << json::parse(line) << std::endl;
-    // }
     // return j.at("ClassName");
 
-    for (std::string tmpBuff; std::getline(dbStream, tmpBuff);)
-    {
-        tmpBuff.erase(remove(tmpBuff.begin(), tmpBuff.end(), '\"'), tmpBuff.end());
-        // std::cout << tmpBuff << std::endl;
-        if (tmpBuff.find("mDisplayName") != std::string::npos)
-        {
-            return tmpBuff;
-        }
-    }
+    // for (std::string tmpBuff; std::getline(dbStream, tmpBuff);)
+    // {
+    //     tmpBuff.erase(remove(tmpBuff.begin(), tmpBuff.end(), '\"'), tmpBuff.end());
+    //     // std::cout << tmpBuff << std::endl;
+    //     if (tmpBuff.find("mDisplayName") != std::string::npos)
+    //     {
+    //         return tmpBuff;
+    //     }
+    // }
 
-    return "";
+    // return "";
+    return nameItemStr.empty() ? throw std::invalid_argument("No Item found. Check file format, content and encoding")
+                               : nameItemStr;
 }
 
 std::string Parser::getRecipe()
